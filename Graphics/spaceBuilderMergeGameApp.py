2da342cf5bf07c006b3cbc0beda7df2ee0f253ad
jj b <jj@thebabers.at>
2021-04-05 17:49:53 +0100

    fixed logger

--- Graphics/spaceBuilderMergeGameApp.py
+++ Graphics/spaceBuilderMergeGameApp.py
@@ -1,8 +1,11 @@
 from kivy.app import App
 
 from Graphics.spaceBuilderMergeGameScreenManager import SpaceBuilderMergeGameScreenManager
+from misc.betterLogger import BetterLogger
 
 
-class SpaceBuilderMergeGameApp(App):
+class SpaceBuilderMergeGameApp(App, BetterLogger):
     def build(self):
+        self.log_info("Building app")
+
         return SpaceBuilderMergeGameScreenManager()
--- misc/betterLogger.py
+++ misc/betterLogger.py
@@ -1,16 +1,15 @@
 from kivy.logger import Logger as _Logger
 
+
 class BetterLogger:
     def log_debug(self, *args):
-        _Logger.debug(str(self.__name__) + ": " + "".join([str(arg) for arg in args]))
+        _Logger.debug(str(self.__class__.__name__) + ": " + "".join([str(arg) for arg in args]))
 
     def log_info(self, *args):
-        _Logger.info(str(self.__name__) + ": " + "".join([str(arg) for arg in args]))
+        _Logger.info(str(self.__class__.__name__) + ": " + "".join([str(arg) for arg in args]))
 
     def log_warning(self, *args):
-        _Logger.warning(str(self.__name__) + ": " + "".join([str(arg) for arg in args]))
+        _Logger.warning(str(self.__class__.__name__) + ": " + "".join([str(arg) for arg in args]))
 
     def log_critical(self, *args):
-        _Logger.critical(str(self.__name__) + ": " + "".join([str(arg) for arg in args]))
-
-
+        _Logger.critical(str(self.__class__.__name__) + ": " + "".join([str(arg) for arg in args]))
