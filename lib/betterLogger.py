2d41e19626b68d6c70a6a2eb297ca917b304ded3
jj b <jj@thebabers.at>
2021-04-15 17:29:23 +0100

    fixed logger so it now puts spaces betewwn logs!

--- lib/betterLogger.py
+++ lib/betterLogger.py
@@ -5,16 +5,16 @@ from kivy.logger import Logger as _Logger
 
 class BetterLogger:
     def log_debug(self, *args):
-        _Logger.debug(str(self.__class__.__name__) + ": " + "".join([str(arg) for arg in args]))
+        _Logger.debug(str(self.__class__.__name__) + ": " + " ".join([str(arg) for arg in args]))
 
     def log_info(self, *args):
-        _Logger.info(str(self.__class__.__name__) + ": " + "".join([str(arg) for arg in args]))
+        _Logger.info(str(self.__class__.__name__) + ": " + " ".join([str(arg) for arg in args]))
 
     def log_warning(self, *args):
-        _Logger.warning(str(self.__class__.__name__) + ": " + "".join([str(arg) for arg in args]))
+        _Logger.warning(str(self.__class__.__name__) + ": " + " ".join([str(arg) for arg in args]))
 
     def log_critical(self, *args):
-        _Logger.critical(str(self.__class__.__name__) + ": " + "".join([str(arg) for arg in args]))
+        _Logger.critical(str(self.__class__.__name__) + ": " + " ".join([str(arg) for arg in args]))
 
 
 def redo_logger_formatting():
