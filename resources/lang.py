b6d4291bbbdd58d4deec53cae55be7f200d4d90b
jj b <GreenJon902@gmail.com>
2021-05-15 16:47:30 +0100

    added germanm language and made lang lowercase

--- resources/lang.py
+++ resources/lang.py
@@ -1,49 +1,35 @@
 from kivy.event import EventDispatcher
 from kivy.properties import OptionProperty
 
+from configurables import userSettings
 from lib.betterLogger import BetterLogger
 
 #  All locale codes
 # noinspection SpellCheckingInspection
-language_codes: list[str] = ["af-ZA", "am-ET", "ar-AE", "ar-BH", "ar-DZ", "ar-EG", "ar-IQ", "ar-JO", "ar-KW", "ar-LB",
-                             "ar-LY",
-                             "ar-MA", "arn-CL", "ar-OM", "ar-QA", "ar-SA", "ar-SY", "ar-TN", "ar-YE", "as-IN",
-                             "az-Cyrl-AZ",
-                             "az-Latn-AZ", "ba-RU", "be-BY", "bg-BG", "bn-BD", "bn-IN", "bo-CN", "br-FR", "bs-Cyrl-BA",
-                             "bs-Latn-BA", "ca-ES", "co-FR", "cs-CZ", "cy-GB", "da-DK", "de-AT", "de-CH", "de-DE",
-                             "de-LI",
-                             "de-LU", "dsb-DE", "dv-MV", "el-GR", "en-029", "en-AU", "en-BZ", "en-CA", "en-GB", "en-IE",
-                             "en-IN", "en-JM", "en-MY", "en-NZ", "en-PH", "en-SG", "en-TT", "en-US", "en-ZA", "en-ZW",
-                             "es-AR",
-                             "es-BO", "es-CL", "es-CO", "es-CR", "es-DO", "es-EC", "es-ES", "es-GT", "es-HN", "es-MX",
-                             "es-NI",
-                             "es-PA", "es-PE", "es-PR", "es-PY", "es-SV", "es-US", "es-UY", "es-VE", "et-EE", "eu-ES",
-                             "fa-IR",
-                             "fi-FI", "fil-PH", "fo-FO", "fr-BE", "fr-CA", "fr-CH", "fr-FR", "fr-LU", "fr-MC", "fy-NL",
-                             "ga-IE",
-                             "gd-GB", "gl-ES", "gsw-FR", "gu-IN", "ha-Latn-NG", "he-IL", "hi-IN", "hr-BA", "hr-HR",
-                             "hsb-DE",
-                             "hu-HU", "hy-AM", "id-ID", "ig-NG", "ii-CN", "is-IS", "it-CH", "it-IT", "iu-Cans-CA",
-                             "iu-Latn-CA",
-                             "ja-JP", "ka-GE", "kk-KZ", "kl-GL", "km-KH", "kn-IN", "kok-IN", "ko-KR", "ky-KG", "lb-LU",
-                             "lo-LA",
-                             "lt-LT", "lv-LV", "mi-NZ", "mk-MK", "ml-IN", "mn-MN", "mn-Mong-CN", "moh-CA", "mr-IN",
-                             "ms-BN",
-                             "ms-MY", "mt-MT", "nb-NO", "ne-NP", "nl-BE", "nl-NL", "nn-NO", "nso-ZA", "oc-FR", "or-IN",
-                             "pa-IN",
-                             "pl-PL", "prs-AF", "ps-AF", "pt-BR", "pt-PT", "qut-GT", "quz-BO", "quz-EC", "quz-PE",
-                             "rm-CH",
-                             "ro-RO", "ru-RU", "rw-RW", "sah-RU", "sa-IN", "se-FI", "se-NO", "se-SE", "si-LK", "sk-SK",
-                             "sl-SI",
-                             "sma-NO", "sma-SE", "smj-NO", "smj-SE", "smn-FI", "sms-FI", "sq-AL", "sr-Cyrl-BA",
-                             "sr-Cyrl-CS",
-                             "sr-Cyrl-ME", "sr-Cyrl-RS", "sr-Latn-BA", "sr-Latn-CS", "sr-Latn-ME", "sr-Latn-RS",
-                             "sv-FI", "sv-SE",
-                             "sw-KE", "syr-SY", "ta-IN", "te-IN", "tg-Cyrl-TJ", "th-TH", "tk-TM", "tn-ZA", "tr-TR",
-                             "tt-RU",
-                             "tzm-Latn-DZ", "ug-CN", "uk-UA", "ur-PK", "uz-Cyrl-UZ", "uz-Latn-UZ", "vi-VN", "wo-SN",
-                             "xh-ZA",
-                             "yo-NG", "zh-CN", "zh-HK", "zh-MO", "zh-SG", "zh-TW", "zu-ZA"]
+language_codes: list[str] = ['af-za', 'am-et', 'ar-ae', 'ar-bh', 'ar-dz', 'ar-eg', 'ar-iq', 'ar-jo', 'ar-kw', 'ar-lb',
+                             'ar-ly', 'ar-ma', 'arn-cl', 'ar-om', 'ar-qa', 'ar-sa', 'ar-sy', 'ar-tn', 'ar-ye', 'as-in',
+                             'az-cyrl-az', 'az-latn-az', 'ba-ru', 'be-by', 'bg-bg', 'bn-bd', 'bn-in', 'bo-cn', 'br-fr',
+                             'bs-cyrl-ba', 'bs-latn-ba', 'ca-es', 'co-fr', 'cs-cz', 'cy-gb', 'da-dk', 'de-at', 'de-ch',
+                             'de-de', 'de-li', 'de-lu', 'dsb-de', 'dv-mv', 'el-gr', 'en-029', 'en-au', 'en-bz', 'en-ca',
+                             'en-gb', 'en-ie', 'en-in', 'en-jm', 'en-my', 'en-nz', 'en-ph', 'en-sg', 'en-tt', 'en-us',
+                             'en-za', 'en-zw', 'es-ar', 'es-bo', 'es-cl', 'es-co', 'es-cr', 'es-do', 'es-ec', 'es-es',
+                             'es-gt', 'es-hn', 'es-mx', 'es-ni', 'es-pa', 'es-pe', 'es-pr', 'es-py', 'es-sv', 'es-us',
+                             'es-uy', 'es-ve', 'et-ee', 'eu-es', 'fa-ir', 'fi-fi', 'fil-ph', 'fo-fo', 'fr-be', 'fr-ca',
+                             'fr-ch', 'fr-fr', 'fr-lu', 'fr-mc', 'fy-nl', 'ga-ie', 'gd-gb', 'gl-es', 'gsw-fr', 'gu-in',
+                             'ha-latn-ng', 'he-il', 'hi-in', 'hr-ba', 'hr-hr', 'hsb-de', 'hu-hu', 'hy-am', 'id-id',
+                             'ig-ng', 'ii-cn', 'is-is', 'it-ch', 'it-it', 'iu-cans-ca', 'iu-latn-ca', 'ja-jp', 'ka-ge',
+                             'kk-kz', 'kl-gl', 'km-kh', 'kn-in', 'kok-in', 'ko-kr', 'ky-kg', 'lb-lu', 'lo-la', 'lt-lt',
+                             'lv-lv', 'mi-nz', 'mk-mk', 'ml-in', 'mn-mn', 'mn-mong-cn', 'moh-ca', 'mr-in', 'ms-bn',
+                             'ms-my', 'mt-mt', 'nb-no', 'ne-np', 'nl-be', 'nl-nl', 'nn-no', 'nso-za', 'oc-fr', 'or-in',
+                             'pa-in', 'pl-pl', 'prs-af', 'ps-af', 'pt-br', 'pt-pt', 'qut-gt', 'quz-bo', 'quz-ec',
+                             'quz-pe', 'rm-ch', 'ro-ro', 'ru-ru', 'rw-rw', 'sah-ru', 'sa-in', 'se-fi', 'se-no', 'se-se',
+                             'si-lk', 'sk-sk', 'sl-si', 'sma-no', 'sma-se', 'smj-no', 'smj-se', 'smn-fi', 'sms-fi',
+                             'sq-al', 'sr-cyrl-ba', 'sr-cyrl-cs', 'sr-cyrl-me', 'sr-cyrl-rs', 'sr-latn-ba',
+                             'sr-latn-cs', 'sr-latn-me', 'sr-latn-rs', 'sv-fi', 'sv-se', 'sw-ke', 'syr-sy', 'ta-in',
+                             'te-in', 'tg-cyrl-tj', 'th-th', 'tk-tm', 'tn-za', 'tr-tr', 'tt-ru', 'tzm-latn-dz', 'ug-cn',
+                             'uk-ua', 'ur-pk', 'uz-cyrl-uz', 'uz-latn-uz', 'vi-vn', 'wo-sn', 'xh-za', 'yo-ng', 'zh-cn',
+                             'zh-hk', 'zh-mo', 'zh-sg', 'zh-tw', 'zu-za']
+
 
 
 def convert(array: {str: str}):
@@ -67,6 +53,11 @@ def convert(array: {str: str}):
 class Lang(BetterLogger, EventDispatcher):
     language_code: OptionProperty = OptionProperty("en-gb", options=language_codes)
     languages: {str: {str: str}} = {}
+
+    def __init__(self):
+        BetterLogger.__init__(self)
+        EventDispatcher.__init__(self)
+        self.language_code = userSettings.get("UI", "language")
 
     def register_array(self, array: {str: str}, language_code: str):
         self.log_trace("Registering array for", language_code)
