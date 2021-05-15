24a98c27a25cf49d43e54b4ba67a2d29ebbf7a09
jj b <GreenJon902@gmail.com>
2021-05-15 14:41:20 +0100

    removed vectors and added some __init__ files bc they make it look nicer

---
+++ graphics/customWidgets/__init__.py
@@ -0,0 +1,10 @@
+from graphics.customWidgets.baseLayout import BaseLayout
+from graphics.customWidgets.betterButton import BetterButton
+from graphics.customWidgets.betterScatter import BetterScatter
+from graphics.customWidgets.buildingButtonsHandler import BuildingButtonsHandler
+from graphics.customWidgets.multiLangLabel import MultiLangLabel
+from graphics.customWidgets.noRotationLabel import NoRotationLabel
+from graphics.customWidgets.screenManagerSwitcher import ScreenManagerSwitcher
+
+__all__ = ["BetterButton", "BetterScatter", "BuildingButtonsHandler", "BaseLayout",
+           "MultiLangLabel", "NoRotationLabel", "ScreenManagerSwitcher"]
